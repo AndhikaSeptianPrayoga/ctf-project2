03f5e15beae47896c58704e18999e1474449c393
AkbarWiraN <akbarwiranugraha162@gmail.com>
2024-06-06 20:40:24 +0700

    Memperbarui tampilan index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,7 +40,7 @@
       <div class="container">
           <div class="row">
             <div class="col-xl-6">
-              <h1 class="display-3">CAPTURE AND FLAG<span class="vim-caret">͏͏&nbsp;</span></h1>
+              <h1 class="display-3">CAPTURE THE FLAG<span class="vim-caret">͏͏&nbsp;</span></h1>
               <div class="lead mb-3 text-mono text-success">Welcome hackers! prove the world's most powerful hacking, is here! and prove the world that a computer genius with a laptop is not a nerd sitting in the corner ! join our CTF and expand your knowledge !</div>
               <div class="text-mono">
                 <a href="#"
@@ -62,39 +62,39 @@
           <div class="row text-center">
               <div class="col-12">
                 <h2 class="section-title">Challenges</h2>
-                <p class="section-subtitle">Jelajahi Serunya Tantangan! Temukan Adrenalin yang Tak Tergantikan dalam Menyelesaikan Setiap Masalah. Yuk, Segera Hadapi Challanges Kami dan Buktikan Kemampuanmu!</p>
+                <p class="section-subtitle">Category</p>
               </div>
             </div>
             <div class="row">
               <div class="col-md-3">
                 <div class="card">
                   <div class="card-body">
-                    <h5 class="card-title"><i class="fas fa-laptop-code"></i> Binary</h5>
-                    <p class="card-text">Binary adalah salah satu challenge yang diberikan oleh tim CTF.</p>
+                    <h5 class="card-title"><i class="fas fa-laptop-code"></i>Web Exploit</h5>
+                    <p class="card-text">Find a vulnerability & exploit</p>
                   </div>
                 </div>
               </div>
               <div class="col-md-3">
                 <div class="card">
                   <div class="card-body">
-                    <h5 class="card-title"><i class="fas fa-users"></i> Pramuka</h5>
-                    <p class="card-text">Pramuka adalah salah satu challenge yang diberikan oleh tim CTF.</p>
+                    <h5 class="card-title"><i class="fas fa-users"></i>Cryptography</h5>
+                    <p class="card-text">Find a Encryption</p>
                   </div>
                 </div>
               </div>
               <div class="col-md-3">
                 <div class="card">
                   <div class="card-body">
-                    <h5 class="card-title"><i class="fas fa-user-secret"></i> Melacak Hackers</h5>
-                    <p class="card-text">Melacak Hackers adalah salah satu challenge yang diberikan oleh tim CTF.</p>
+                    <h5 class="card-title"><i class="fas fa-user-secret"></i>OSINT</h5>
+                    <p class="card-text">Find a Information</p>
                   </div>
                 </div>
               </div>
               <div class="col-md-3">
                 <div class="card">
                   <div class="card-body">
-                    <h5 class="card-title"><i class="fas fa-laptop"></i> Kijang satu</h5>
-                    <p class="card-text">Kijang satu adalah salah satu challenge yang diberikan oleh tim CTF.</p>
+                    <h5 class="card-title"><i class="fas fa-laptop"></i>Programming</h5>
+                    <p class="card-text">Problem Solving</p>
                   </div>
                 </div>
               </div>
@@ -130,7 +130,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
                         <path d="M0 0v24h24v-24h-24zm1 1h11v3.55c0 .932.575 1.357 1.109 1.357.332 0 .672-.156.953-.438.284-.296.389-.469.786-.469.47 0 1.152.534 1.152 1.5s-.682 1.5-1.152 1.5c-.396 0-.501-.173-.785-.468-.281-.283-.621-.438-.953-.438-.535-.001-1.11.424-1.11 1.356v3.55h-3.55c-.536 0-.439-.139-.108-.454.262-.245.658-.616.658-1.394 0-1.039-1.004-2.152-2.5-2.152s-2.5 1.113-2.5 2.152c0 .777.396 1.148.658 1.394.333.317.425.454-.108.454h-3.55v-11zm0 22v-10h3.55c.933 0 1.356-.575 1.356-1.109 0-.332-.155-.672-.438-.953-.294-.284-.468-.388-.468-.786 0-.47.534-1.152 1.5-1.152s1.5.682 1.5 1.152c0 .397-.174.501-.469.785-.282.281-.438.621-.438.953.001.535.425 1.11 1.357 1.11h3.55v3.55c0 .535-.137.44-.454.109-.245-.263-.616-.659-1.394-.659-1.039 0-2.152 1.004-2.152 2.5s1.113 2.5 2.152 2.5c.777 0 1.148-.396 1.394-.659.317-.333.454-.424.454.109v2.55h-11zm22 0h-10v-2.55c0-.932-.575-1.357-1.109-1.357-.332 0-.672.156-.953.438-.284.296-.389.469-.786.469-.47 0-1.152-.534-1.152-1.5s.682-1.5 1.152-1.5c.396 0 .501.173.785.468.281.283.621.438.953.438.534 0 1.109-.425 1.109-1.357v-3.549h3.55c.536 0 .439.139.108.454-.261.245-.657.616-.657 1.394 0 1.039 1.004 2.152 2.5 2.152s2.5-1.113 2.5-2.152c0-.777-.396-1.148-.658-1.394-.333-.317-.425-.454.108-.454h2.55v10zm-2.55-11c-.933 0-1.356.575-1.356 1.109 0 .332.155.672.438.953.294.284.468.388.468.786 0 .47-.534 1.152-1.5 1.152s-1.5-.682-1.5-1.152c0-.397.174-.501.469-.785.282-.281.438-.621.438-.953 0-.534-.424-1.109-1.356-1.109h-3.551v-3.551c0-.535.137-.44.454-.109.245.263.616.659 1.394.659 1.039 0 2.152-1.004 2.152-2.5s-1.113-2.5-2.152-2.5c-.777 0-1.148.396-1.394.659-.317.333-.454.424-.454-.109v-3.55h10v11h-2.55z"/></svg>
                 </div>
-          <h3>Challanges</h3>
+          <h3>Challenges</h3>
           <p>Dari kode yang rumit hingga misteri yang menarik, hadapi tantangan beragam kami untuk mengasah keahlian Anda dalam keamanan cyber</p>
         </div>
       </div>
@@ -201,7 +201,7 @@
                   <img class="card-img-top" src="{{ asset('img/riza.jpg') }}" alt="Rija">
                   <div class="card-body text-center">
                     <h5 class="card-title">M Riza Buccharelli</h5>
-                    <p class="card-text">UI/UX Designer</p>
+                    <p class="card-text">Full Stack Developer</p>
                     <div class="social-icons">
                       <a href="#"><i class="fab fa-twitter"></i></a>
                       <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -218,7 +218,7 @@
                   <img class="card-img-top" src="{{ asset('img/akbar.jpeg') }}" alt="Abay">
                   <div class="card-body text-center">
                     <h5 class="card-title">Akbar Wira Nugraha</h5>
-                    <p class="card-text">Back end Developer</p>
+                    <p class="card-text">CyberSec Enthusiast</p>
                     <div class="social-icons">
                       <a href="#"><i class="fab fa-twitter"></i></a>
                       <a href="#"><i class="fab fa-facebook-f"></i></a>
