a5cc0c2297328e1713df662215f1db7be56ac38d
Andhika Septian Prayoga <andhikaseptianprayoga@upi.edu>
2024-06-06 17:04:27 +0700

    update

--- public/main.blade.php
+++ public/main.blade.php
@@ -7,9 +7,9 @@
     <title>PICT - CTF</title>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap4-neon-glow.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/particles.css') }}">
+    <link rel="stylesheet" href="css/bootstrap4-neon-glow.min.css">
+    <link rel="stylesheet" href="css/user.css">
+    <link rel="stylesheet" href="css/particles.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/font-hack/2.020/css/hack.min.css'>
     <script defer   src="../Js/main.js"></script>
@@ -22,7 +22,7 @@
     <nav class="main-menu">
           <div>
             <div class="user-info">
-                <img src="{{ asset('img/sample_placeholder.png') }}" alt="">
+                <img src="img/sample_placeholder.png" alt="">
               <p>Name User</p>
             </div>
             <ul>
@@ -131,8 +131,8 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/particles.js') }}"></script>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="js/particles.js"></script>
+        <script src="js/app.js"></script>
       </body>
 </html>
 
