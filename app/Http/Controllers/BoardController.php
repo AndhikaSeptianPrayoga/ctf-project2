a029ee00121c7e632806d92e09b594a79b260b41
Mangriza <rizabucarelli@gmail.com>
2024-06-10 08:56:23 +0700

    updet

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class BoardController extends Controller
+{
+    public function index()
+    {
+        $rankings = User::select('users.id_user', 'users.username', 
+                DB::raw('SUM(challenges.poin) as total_points'))
+            ->join('solves', 'users.id_user', '=', 'solves.id_user')
+            ->join('challenges', 'solves.id_chall', '=', 'challenges.id_chall')
+            ->where('solves.status', 1)
+            ->groupBy('users.id_user', 'users.username')
+            ->orderByDesc('total_points')
+            ->get();
+
+        
+        return view('board-user', compact('rankings'));
+      
+    }
+}
